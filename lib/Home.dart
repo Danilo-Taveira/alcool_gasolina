8789ab17c0e76d12297d19c0a92ad8190562e7bb
Danilo-Taveira <dtnavega@gmail.com>
2024-02-19 15:55:17 -0300

    projeto final

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,23 +8,108 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final TextEditingController _controllerAlcool = TextEditingController();
+  final TextEditingController _controllerGasolina = TextEditingController();
+  String _textResultado = '';
+
+  void _calcular() {
+    double? precoAlcool = double.tryParse(_controllerAlcool.text);
+    double? precoGasolina = double.tryParse(_controllerGasolina.text);
+
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _textResultado = 'Número inválido, digite números maiores que 0 e utilizando (.)';
+      });
+    } else {
+      if ((precoAlcool / precoGasolina) >= 0.7) {
+        setState(() {
+          _textResultado = 'Melhor abastecer com gasolina';
+        });
+      } else {
+        setState(() {
+          _textResultado = 'Melhor abastecer com álcool';
+        });
+      }
+      _limparCampos();
+    }
+  }
+
+  void _limparCampos() {
+    _controllerAlcool.text = '';
+    _controllerGasolina.text = '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Álcool ou Gasolina'),
+        title: const Text('Álcool ou Gasolina'),
         backgroundColor: Colors.blue,
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(32),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Padding(
-              padding: EdgeInsets.all(32),
+              padding: const EdgeInsets.only(bottom: 32),
+              child: Image.asset('images/logo.png'),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(bottom: 10),
               child: Text(
                 'Saiba qual a melhor opção para abastecimento do seu carro',
                 style: TextStyle(
                   fontSize: 25,
-                  fontWeight: FontWeight.bold,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Preço Álcool, ex: 1.59'
+              ),
+              style: const TextStyle(
+                fontSize: 22
+              ),
+              controller: _controllerAlcool,
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Preço Gasolina, ex: 3.59'
+              ),
+              style: const TextStyle(
+                fontSize: 22
+              ),
+              controller: _controllerGasolina,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: ElevatedButton(
+                onPressed: _calcular,
+                style: ButtonStyle(
+                  minimumSize: MaterialStateProperty.all(const Size(double.infinity, 50)),
+                  backgroundColor: MaterialStateProperty.all(Colors.blue)
+                ),
+                child: const Text(
+                  'Calcular',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                _textResultado,
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold
                 ),
               ),
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:alcool_gasolina/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Home(),
   ));
 }
